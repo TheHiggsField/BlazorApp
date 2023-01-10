cdfa82fac4f4b50507fad02811cbbf01b046f9bc
Rune Lau Riksted <rlr@commentor.dk>
2023-01-10 14:10:29 +0100

    Added a login test for azureAD

---
+++ Pages/AzureADLogin.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+
+namespace AzureADLogin
+{
+    class PseudoPrincipal
+    {
+        private const string _clientId = "e1256751-d79e-4efc-8c16-90676b30091e";
+        private const string _tenantId = "635aa01e-f19d-49ec-8aed-4b2e4312a627";
+
+        public static async Task LoginAD()
+        {
+            var app = PublicClientApplicationBuilder
+                .Create(_clientId)
+                .WithAuthority(AzureCloudInstance.AzurePublic, _tenantId)
+                .WithRedirectUri("https://azure.riksted.eu/counter")
+                .Build(); 
+            string[] scopes = {  };
+            AuthenticationResult result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
+
+            Console.WriteLine($"Token:\t{result.AccessToken}");
+        }
+    }
+}
