187c8ea28bdfcfddfc99102678ccc0c8016ac1c1
Rune Lau Riksted <rlr@commentor.dk>
2023-01-13 15:51:37 +0100

    Now able to get token for the logedin user :-)

--- Pages/AzureADLogin.cs
+++ Pages/AzureADLogin.cs
@@ -1,6 +1,16 @@
 using System;
 using System.Threading.Tasks;
 using Microsoft.Identity.Client;
+using Azure;
+using System.Text;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Azure.Identity;
+using System.Threading.Tasks;
+using Microsoft.Identity.Web;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Primitives;
 
 namespace AzureADLogin
 {
@@ -8,18 +18,57 @@ namespace AzureADLogin
     {
         private const string _clientId = "c79dd058-f5e8-4e61-8ce1-5dc194ba77b1";
         private const string _tenantId = "fdaef475-4076-49a6-afec-94a2a319d8db";
+        private string[] scopes = { "User.Read", "https://csb100320026571cde5.blob.core.windows.net/user_impersonation" } ;
 
-        public static async Task LoginAD(string uri)
-        {
-            var app = PublicClientApplicationBuilder
+        
+        IConfidentialClientApplication? app {get; set;} = null;
+
+        public async Task<Uri> GetAuthorizationRequestUrl(string uriString)
+        {           
+            System.Uri uri = new System.Uri(uriString);
+            //Microsoft.Identity.Client.IAccount derp = GetAuthorizationRequestUrlParameterBuilder
+            app = ConfidentialClientApplicationBuilder
                 .Create(_clientId)
-                .WithAuthority(AzureCloudInstance.AzurePublic, _tenantId)
-                .WithRedirectUri(uri)
+                .WithTenantId(_tenantId)
+                .WithClientSecret("NJR8Q~mw9R1cB5SjsnNaoI1sB15jEai8m9tOtbg1")
+                .WithRedirectUri(uri.GetLeftPart(UriPartial.Path) )
                 .Build();
-            string[] scopes = { "https://csb100320026571cde5.blob.core.windows.net/user_impersonation" };
-            AuthenticationResult result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
             
-            Console.WriteLine($"Token:\t{result.AccessToken}");
+            //var accounts = await app.GetAccountsAsync();
+
+            var AuthURI = await app.GetAuthorizationRequestUrl( scopes).ExecuteAsync();
+            return AuthURI;
+        }
+        
+        public string? GetCodeFromUri(string uriString)
+        {   
+            
+
+            System.Uri uri = new System.Uri(uriString);
+            app = ConfidentialClientApplicationBuilder
+                .Create(_clientId)
+                .WithTenantId(_tenantId)
+                .WithClientSecret("NJR8Q~mw9R1cB5SjsnNaoI1sB15jEai8m9tOtbg1")
+                .WithRedirectUri(uri.GetLeftPart(UriPartial.Path))
+                .Build();
+            return QueryHelpers.ParseQuery(uri.Query)?["code"];
+        }
+
+        public async Task<AuthenticationResult?> GetTokenFromCode(string? authZCode)
+        {
+           
+
+            if (app == null || authZCode == null )
+            {
+                Console.WriteLine($"Token:app = {app}\nauthZCode\t{authZCode}");
+                return null;
+            }
+            
+            AuthenticationResult result = await app.AcquireTokenByAuthorizationCode(scopes, authZCode ).ExecuteAsync();
+            
+            Console.WriteLine($"Account:\t{result.Account}\n Token:\t{result.AccessToken}");
+
+            return result;
         }
     }
 }
